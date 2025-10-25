d0eb0595a802eb2c731f2bb1fe618d73b2dd4add
Matheus Fernandes Escobedo <matheus9762@gmail.com>
2025-10-25 12:22:09 -0300

    feat: add get tickers from yfinance

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import os
 import time
 import schedule
+import yfinance as yf
+from dotenv import load_dotenv
 
+load_dotenv()
+
+env_ticker = os.getenv("TICKER")
+env_period = os.getenv("TICKER_PERIOD")
+env_interval = os.getenv("TICKER_INTERVAL")
+env_last_count = os.getenv("TICKER_LAST_COUNT")
 
 def main():
-    print("Tarefa executada a cada 10s.")
+    ticker = yf.Ticker(env_ticker)
+    ticker_history = ticker.history(period=env_period, interval=env_interval)
+    print(f"\n--- {env_ticker} ---")
+    print(ticker_history.tail(int(env_last_count)))
 
 schedule.every(10).seconds.do(main)
 
